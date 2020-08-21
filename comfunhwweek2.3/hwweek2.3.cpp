56b48e7d54aa742bd8129a6f76ca2f62eecbabbc
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-08-21 15:26:24 +0700

    Add project files.

---
+++ comfunhwweek2.3/hwweek2.3.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main() {
+	int x;
+	printf("Enter number : ");
+	scanf("%d", &x);
+	for (int i = 0; i <= x; i++) {
+		printf("*");
+	}
+	for (int j = 1; j <= x; j++) {
+		printf("\n*");
+		for (int k = 2; k <= x; k++) {
+			printf(" ");
+		}
+		printf("*");
+	}
+	printf("\n");
+	for (int y = 0; y <= x; y++) {
+		printf("*");
+	}
+}
