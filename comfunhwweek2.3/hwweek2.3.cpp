c867ccaad37c5d708c98b62beda957b511a68deb
PHATTHARANIT TESJAROEN <63010727@KMITL.AC.TH>
2020-08-28 20:50:55 +0700

    Round2

--- comfunhwweek2.3/hwweek2.3.cpp
+++ comfunhwweek2.3/hwweek2.3.cpp
@@ -1,21 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+
 int main() {
-	int x;
-	printf("Enter number : ");
-	scanf("%d", &x);
-	for (int i = 0; i <= x; i++) {
-		printf("*");
-	}
-	for (int j = 1; j <= x; j++) {
-		printf("\n*");
-		for (int k = 2; k <= x; k++) {
-			printf(" ");
-		}
-		printf("*");
-	}
-	printf("\n");
-	for (int y = 0; y <= x; y++) {
-		printf("*");
-	}
-}   
+    int x;
+    printf("Enter number : ");
+    scanf("%d", &x);
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < x; j++) {
+            if ((i == 0 || i == x - 1) || (j == 0 || j == x - 1))printf("*");
+            else printf(" ");
+        }printf("\n");
+    }
+    return 0;
+}
+//ส่งอีกรอบ round2
